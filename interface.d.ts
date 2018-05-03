d7af2a9baed9e88f4538aa27d9832130901d1842
PHUA XUE YONG _ <xyphua.2014@smu.edu.sg>
2018-05-03 15:45:14 +0800

    updated the interface so we can check on types

---
+++ interface.d.ts
@@ -0,0 +1,12 @@
+interface weatherDetails {
+    temp: number,
+    apparentTemp: number,
+    hourlySummary: string,
+    hourlyIcon: string,
+    dailySummary: string,
+    dailyIcon: string,
+}
+
+interface emoji {
+    content: string,
+}
